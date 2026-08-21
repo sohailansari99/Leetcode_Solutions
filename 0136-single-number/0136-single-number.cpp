3d05fc553fe80e027173484ec48b2a09c90189f1
Sohail Ansari <sohailjustin40@gmail.com>
2026-08-21 22:25:59 +0530

    Time: 15 ms (9.48%), Space: 24 MB (7.5%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> freq;
+        for(int x : nums){
+            freq[x]++;
+        }
+        for(int x : nums){
+            if(freq[x] == 1){
+                return x;
+            }
+        }
+        return -1;
+    }
+};
